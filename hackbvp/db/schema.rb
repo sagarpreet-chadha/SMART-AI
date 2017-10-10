df8691399091e948186dd929f760ca8733be2dbb
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2017-10-10 17:12:43 +0530

    Paperclips added for images

--- hackbvp/db/schema.rb
+++ hackbvp/db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010102555) do
+ActiveRecord::Schema.define(version: 20171010110554) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "votes"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
