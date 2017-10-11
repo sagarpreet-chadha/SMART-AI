6308de46efdd842c456cfbdebd02544600bc51f0
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2017-10-11 06:51:21 +0530

    WEBSITE ADDED

--- hackbvp/config/routes.rb
+++ hackbvp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'news/index' => "news#index" 
+  get 'news/index' => "news#index" , as: :news_index
   post 'news/vote/:id' => "news#vote" , as: :news_vote 
   post 'news/devote/:id' => "news#devote" , as: :news_devote 
 
